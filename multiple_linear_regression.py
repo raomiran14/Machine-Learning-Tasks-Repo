b4fdfd203f6e0ad8892e81810c12e905ee0faaa1
raomiran14 <raomieran@gmail.com>
2025-10-07 14:56:04 -0700

    Step 1: Load and inspect dataset

---
+++ multiple_linear_regression.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+dataset=pd.read_csv("multiple_linear_regression_dataset.csv")
+print(dataset.head(3))
+print(dataset.shape)
+print(dataset.isnull().sum())
+sns.pairplot(data=dataset)
+#plt.show()
+x=dataset[["age","experience"]]
+y=dataset["income"]
+print(x.ndim)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+from sklearn.linear_model import LinearRegression
+lr=LinearRegression()
+lr.fit(x_train,y_train)
+print(lr.score(x_test,y_test)*100)
+print(lr.predict(x_test))
