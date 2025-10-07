3f6fcdec113402b9c2640754e60ad0239fdfd2b5
raomiran14 <raomieran@gmail.com>
2025-10-07 15:10:27 -0700

    Add model training and prediction

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -7,3 +7,12 @@ print(dataset.shape)
 print(dataset.isnull().sum())
 sns.pairplot(data=dataset)
 plt.show()
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+x = dataset[["age", "experience"]]
+y = dataset["income"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+print(lr.score(x_test, y_test) * 100)
+print(lr.predict(x_test))
