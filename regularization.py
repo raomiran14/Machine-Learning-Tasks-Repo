8ab4d84be60bdd469c8cb703711ed2140f485e9e
raomiran14 <raomieran@gmail.com>
2025-10-18 11:44:45 -0700

    updated code and tested visualization for regularization models

--- regularization.py
+++ regularization.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns 
 
-# Step 1: Load the dataset
+
 dataset = pd.read_csv("house_price_regression_dataset.csv") 
 print(dataset.head(3))      
 
