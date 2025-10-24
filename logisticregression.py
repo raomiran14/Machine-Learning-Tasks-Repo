2fd46d59400380cfd64a74951498905a446d6d99
raomiran14 <raomieran@gmail.com>
2025-10-24 14:10:12 -0700

    implemented train-test split for logistic regression model

--- logisticregression.py
+++ logisticregression.py
@@ -9,3 +9,9 @@ print(dataset.head(3))
 plt.figure(figsize=(4,3))
 sns.scatterplot(x="Age", y="Purchased", data=dataset)
 plt.show()
+
+x = dataset[["Age"]]
+y = dataset["Purchased"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
