a932f867aa22971cd6bfec321869654dbe79ef1e
raomiran14 <raomieran@gmail.com>
2025-10-26 03:43:10 -0700

    prepare features and split data into train/test

--- logisticregress2.py
+++ logisticregress2.py
@@ -7,6 +7,13 @@ print(dataset.head(3))
 
 plt.figure(figsize=(5,4))
 sns.scatterplot(x="cgpa", y="placement_exam_marks", data=dataset, hue="placed")
- plt.show()
+plt.show()
+
+x = dataset.iloc[:, :-1]
+y = dataset["placed"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
 
 #this code is of logistic regression when we have multiple inputs and are linearly seperated but here the dataset which we use is not linearly seperated but if we use linearly seperated one  it is better 
