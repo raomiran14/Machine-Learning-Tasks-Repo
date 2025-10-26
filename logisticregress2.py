406a4e1539143091f06df687610f1995de1186f5
raomiran14 <raomieran@gmail.com>
2025-10-26 03:43:48 -0700

    train logistic regression and print accuracy & single prediction

--- logisticregress2.py
+++ logisticregress2.py
@@ -15,5 +15,13 @@ y = dataset["placed"]
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+
+print(lr.score(x_test, y_test) * 100)
+print(lr.predict([[7.19, 26.0]]))
+
 
 #this code is of logistic regression when we have multiple inputs and are linearly seperated but here the dataset which we use is not linearly seperated but if we use linearly seperated one  it is better 
