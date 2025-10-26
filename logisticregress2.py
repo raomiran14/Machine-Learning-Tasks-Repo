e469d141f236b7cbf5f0e576e1956887e6bdd278
raomiran14 <raomieran@gmail.com>
2025-10-26 03:42:10 -0700

    load dataset and plot cgpa vs placement marks

---
+++ logisticregress2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv("placement.csv")
+print(dataset.head(3))
+
+plt.figure(figsize=(5,4))
+sns.scatterplot(x="cgpa", y="placement_exam_marks", data=dataset, hue="placed")
+ plt.show()
+
+#this code is of logistic regression when we have multiple inputs and are linearly seperated but here the dataset which we use is not linearly seperated but if we use linearly seperated one  it is better 
