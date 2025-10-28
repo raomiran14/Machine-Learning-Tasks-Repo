59662a96ffe1b03e2eb2228ccb0c4e0b14c28aa2
raomiran14 <raomieran@gmail.com>
2025-10-28 11:28:06 -0700

    Add scatter plot to visualize relationship between CGPA and placement exam marks

--- logreg3.py
+++ logreg3.py
@@ -5,4 +5,8 @@ import seaborn as sns
 dataset = pd.read_csv("placement.csv")
 print(dataset.head(3))
 
+plt.figure(figsize=(5,4))
+sns.scatterplot(x="cgpa", y="placement_exam_marks", data=dataset, hue="placed")
+plt.show()
+
 #if our data is not linearly seperable than we will use polynomial features
