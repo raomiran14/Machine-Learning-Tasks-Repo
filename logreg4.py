d31fd3fda5ef3d10c55476412f088838e3972399
raomiran14 <raomieran@gmail.com>
2025-10-29 11:31:27 -0700

    Train and evaluate Logistic Regression models (OVR & Multinomial)

--- logreg4.py
+++ logreg4.py
@@ -15,3 +15,15 @@ y = dataset["species"]
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+from sklearn.linear_model import LogisticRegression
+
+# One-vs-Rest (OVR)
+lr = LogisticRegression(multi_class="ovr")
+lr.fit(x_train, y_train)
+print(lr.score(x_test, y_test) * 100)
+
+# Multinomial
+lr1 = LogisticRegression(multi_class="multinomial")
+lr1.fit(x_train, y_train)
+print(lr1.score(x_test, y_test) * 100)
+
