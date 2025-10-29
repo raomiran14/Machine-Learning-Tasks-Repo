a40f2f34827faba82c03d841f51bdd6e3ccdc10c
raomiran14 <raomieran@gmail.com>
2025-10-29 11:30:37 -0700

    Split dataset into training and testing sets

--- logreg4.py
+++ logreg4.py
@@ -8,3 +8,10 @@ print(dataset["species"].unique())
 
 sns.pairplot(data=dataset, hue="species")
 plt.show()
+
+x = dataset.iloc[:, :-1]
+y = dataset["species"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
