abf1591741488bc27b29aa30407fce42ae7458e3
raomiran14 <raomieran@gmail.com>
2025-11-10 10:38:48 -0800

    Split data into training and testing sets

--- imbalanced_dataset.py
+++ imbalanced_dataset.py
@@ -6,3 +6,7 @@ print(dataset["Purchased"].value_counts())
 
 x = dataset.iloc[:, :-1]
 y = dataset["Purchased"]
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
