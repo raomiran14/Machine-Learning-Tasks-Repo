e8e92cb61bea54b67c1961bb04d91e8b96c7b31f
raomiran14 <raomieran@gmail.com>
2025-11-15 12:11:52 -0800

    Apply Random Oversampling and train Logistic Regression

--- balancing_dataset.py
+++ balancing_dataset.py
@@ -6,3 +6,20 @@ print(dataset["Purchased"].value_counts())
 
 x = dataset.iloc[:, :-1]
 y = dataset["Purchased"]
+
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+
+ro = RandomOverSampler()
+ro_x, ro_y = ro.fit_resample(x, y)
+print(ro_y.value_counts())
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(ro_x, ro_y, test_size=0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+
+print(lr.score(x_test, y_test) * 100)
+print(lr.predict([[29, 80000]]))
