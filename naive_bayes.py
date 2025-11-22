dee21af7284c4adfeb15e9073e3f4a69573c7042
raomiran14 <raomieran@gmail.com>
2025-11-22 02:43:29 -0800

    Train and evaluate GaussianNB model + plot decision boundary

--- naive_bayes.py
+++ naive_bayes.py
@@ -21,3 +21,13 @@ y = dataset["placed"]
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
+
+gnb = GaussianNB()
+gnb.fit(x_train, y_train)
+
+print(gnb.score(x_test, y_test) * 100), print(gnb.score(x_train, y_train) * 100)
+
+plot_decision_regions(x.to_numpy(), y.to_numpy(), clf=gnb)
+plt.show()
