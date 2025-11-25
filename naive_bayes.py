f7abaaae10f7cd0d20e60e174d9c3a88bb38a15f
raomiran14 <raomieran@gmail.com>
2025-11-25 10:54:23 -0800

    fix the error

--- naive_bayes.py
+++ naive_bayes.py
@@ -20,7 +20,7 @@ x = dataset.iloc[:, :-1]
 y = dataset["placed"]
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=42)
 
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 
