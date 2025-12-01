51c0e42d66a5925d3230878d3ee3549c8892aca0
raomiran14 <raomieran@gmail.com>
2025-12-01 10:36:27 -0800

    Create table.py
    Added multiplication table program (for loop + while loop)

---
+++ table.py
@@ -0,0 +1,10 @@
+n=int(input("enter a no"))
+for i in range(1,11):
+    print(f"{n}*{i}={n*i}")
+
+# printing using while loop
+n=int(input("enter a no"))
+i=1
+while(i<11):
+    print(f"{n}*{i}={n*i}")
+    i+=1
