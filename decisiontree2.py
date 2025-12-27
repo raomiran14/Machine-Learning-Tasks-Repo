4134c4eb2167741bc33352e78c6b6671da7ea22f
raomiran14 <raomieran@gmail.com>
2025-12-27 11:07:19 -0800

    Prepare features and split data into training and testing sets

--- decisiontree2.py
+++ decisiontree2.py
@@ -10,3 +10,9 @@ sns.pairplot(data=dataset)
 # plt.show()
 
 print(dataset.isnull().sum())
+
+x = dataset.iloc[:, :-1]
+y = dataset["income"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
