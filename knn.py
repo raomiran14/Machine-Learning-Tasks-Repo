29ff2f7cd944d84e925d4f4ec50f5f081fa3dff3
raomiran14 <raomieran@gmail.com>
2025-12-28 10:03:54 -0800

    Apply feature scaling and train-test split

--- knn.py
+++ knn.py
@@ -10,3 +10,15 @@ sns.scatterplot(x="Age", y="EstimatedSalary", data=dataset, hue="Purchased")
 plt.show()
 
 print(dataset.isnull().sum())
+
+x = dataset.iloc[:, :-1]
+y = dataset["Purchased"]
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(x)
+x = pd.DataFrame(sc.transform(x), columns=x.columns)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
