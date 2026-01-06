659ebec04597d8915ef8376b44a51eb34b395ec6
raomiran14 <raomieran@gmail.com>
2026-01-06 01:34:07 -0800

    Prepare features and split placement data into train and test sets

--- svm.py
+++ svm.py
@@ -9,3 +9,9 @@ print(dataset.isnull().sum())
 
 sns.scatterplot(x="cgpa", y="placement_exam_marks",data=dataset, hue="placed")
 # plt.show()
+
+x = dataset.iloc[:, :-1]
+y = dataset["placed"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split( x, y, test_size=0.2, random_state=10)
