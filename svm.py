bdc21aec6f4cc6404bd4bf2f3098c060520222da
raomiran14 <raomieran@gmail.com>
2026-01-06 01:33:15 -0800

    Load placement dataset and visualize data

---
+++ svm.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from mlxtend.plotting import plot_decision_regions
+
+dataset = pd.read_csv("D:\PROJECTS\Machine-Learning-Tasks-Repo\placement.csv")
+print(dataset.head(3))
+print(dataset.isnull().sum())
+
+sns.scatterplot(x="cgpa", y="placement_exam_marks",data=dataset, hue="placed")
+# plt.show()
