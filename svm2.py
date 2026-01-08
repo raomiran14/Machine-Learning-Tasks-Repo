a8378cec46cb57280138bdd136eca6c0a7b65c8f
raomiran14 <raomieran@gmail.com>
2026-01-08 10:22:03 -0800

    Prepare features and split placement data for regression

--- svm2.py
+++ svm2.py
@@ -9,3 +9,10 @@ print(dataset.isnull().sum())
 
 sns.scatterplot(x="cgpa", y="package", data=dataset)
 plt.show()
+
+x = dataset[["cgpa"]]
+y = dataset["package"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_testr, y_train, y_test = train_test_split( x, y, test_size=0.2, random_state=42)
+
