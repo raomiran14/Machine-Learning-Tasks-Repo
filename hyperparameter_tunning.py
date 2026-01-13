8025e3a734ffa88173eb87ee958fc465c06b53f2
raomiran14 <raomieran@gmail.com>
2026-01-13 01:05:47 -0800

    Split data and train baseline Decision Tree Regressor

--- hyperparameter_tunning.py
+++ hyperparameter_tunning.py
@@ -5,3 +5,19 @@ print(dataset.head(3))
 
 x = dataset.iloc[:, :-1]
 y = dataset["Salary"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.2, random_state=42
+)
+
+from sklearn.tree import DecisionTreeRegressor
+dt = DecisionTreeRegressor(
+    criterion='squared_error',
+    splitter='best',
+    max_depth=2
+)
+dt.fit(x_train, y_train)
+
+print(dt.score(x_train, y_train) * 100)
+print(dt.score(x_test, y_test) * 100)
