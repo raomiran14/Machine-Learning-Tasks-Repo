fda2e1ae9c6ae592a2e5b902be2df57d8b4f63d1
raomiran14 <raomieran@gmail.com>
2026-01-17 10:27:55 -0800

    Demonstrate Leave-One-Out cross-validation on sample data

--- cross_validation.py
+++ cross_validation.py
@@ -12,3 +12,13 @@ from sklearn.model_selection import cross_val_score, KFold
 p = cross_val_score(LinearRegression(), x, y,  cv=KFold(n_splits=10))
 p.sort()
 print(p * 100)
+
+new_data = dataset.head(10)
+x_new = new_data.iloc[:, :-1]
+y_new = new_data["package"]
+
+from sklearn.model_selection import LeaveOneOut, LeavePOut, KFold, StratifiedKFold
+
+lo = LeaveOneOut()
+for train, test in lo.split(x_new, y_new):
+    print(train, test)
