17762044a660f930e43990d5b548b8ae7cbd195e
raomiran14 <raomieran@gmail.com>
2026-01-17 10:27:13 -0800

    Apply K-Fold cross-validation with Linear Regression

--- cross_validation.py
+++ cross_validation.py
@@ -5,3 +5,10 @@ print(dataset.head(3))
 
 x = dataset.iloc[:, :-1]
 y = dataset["package"]
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score, KFold
+
+p = cross_val_score(LinearRegression(), x, y,  cv=KFold(n_splits=10))
+p.sort()
+print(p * 100)
