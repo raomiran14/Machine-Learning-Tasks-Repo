e35f7bf0e2dcc025744fd4a5b04cfde9be32faa6
raomiran14 <raomieran@gmail.com>
2026-03-26 03:24:52 -0700

    Prepare features and split dataset into training and testing sets

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -13,3 +13,9 @@ print(dataset)
 sns.scatterplot(x="x1", y="x2", data=dataset, hue="y")
 plt.show()
 
+x_a = dataset.iloc[:, :-1]
+y_a = dataset["y"]
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_a, y_a, test_size=0.2, random_state=42)
+
