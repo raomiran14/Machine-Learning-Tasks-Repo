5906e2ec7e570607abdc5e35f4091b9268597fec
raomiran14 <raomieran@gmail.com>
2026-03-26 03:25:21 -0700

    Train Decision Tree, SVM, and Naive Bayes classifiers and evaluate accuracy

--- ensemble_learning.py
+++ ensemble_learning.py
@@ -19,3 +19,18 @@ y_a = dataset["y"]
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_a, y_a, test_size=0.2, random_state=42)
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+
+dt = DecisionTreeClassifier()
+dt.fit(x_train, y_train)
+print(dt.score(x_train, y_train) * 100), print(dt.score(x_test, y_test) * 100)
+
+sv = SVC()
+sv.fit(x_train, y_train)
+print(sv.score(x_train, y_train) * 100), print(sv.score(x_test, y_test) * 100)
+
+gnb = GaussianNB()
+gnb.fit(x_train, y_train)
+print(gnb.score(x_train, y_train) * 100), print(gnb.score(x_test, y_test) * 100)
