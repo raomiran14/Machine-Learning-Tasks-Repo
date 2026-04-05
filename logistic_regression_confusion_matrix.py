473cc3e7e4a6a6a225286038674c105376bbb888
raomiran14 <raomieran@gmail.com>
2026-04-05 11:52:37 -0700

    Train Logistic Regression model and evaluate accuracy

--- logistic_regression_confusion_matrix.py
+++ logistic_regression_confusion_matrix.py
@@ -11,3 +11,11 @@ y = dataset["placed"]
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(  x, y, test_size=0.2, random_state=42)
+
+from sklearn.linear_model import LogisticRegression
+
+lr = LogisticRegression()
+lr.fit(x_train, y_train)
+
+print("Train Accuracy:", lr.score(x_train, y_train) * 100)
+print("Test Accuracy:", lr.score(x_test, y_test) * 100)
