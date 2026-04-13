1b4c615f83ba27be82563f02eb6b930fb305271f
raomiran14 <raomieran@gmail.com>
2026-04-13 11:27:04 -0700

    Apply K-Means clustering and assign cluster labels

--- kmeans_clustering_placement.py
+++ kmeans_clustering_placement.py
@@ -9,3 +9,10 @@ sns.scatterplot(x="cgpa", y="placement_exam_marks", data=dataset)
 plt.show()
 
 x = dataset[["cgpa", "placement_exam_marks"]]
+
+from sklearn.cluster import KMeans
+
+kmeans = KMeans(n_clusters=3)
+dataset["cluster"] = kmeans.fit_predict(x)
+
+print(dataset.head(5))
