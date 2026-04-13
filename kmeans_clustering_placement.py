5c3a55f0e67733a9cee46e00bf3227c84ed6e5bc
raomiran14 <raomieran@gmail.com>
2026-04-13 11:26:06 -0700

    Load dataset and visualize data distribution

---
+++ kmeans_clustering_placement.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv("placement.csv")
+print(dataset.head(3))
+
+sns.scatterplot(x="cgpa", y="placement_exam_marks", data=dataset)
+plt.show()
